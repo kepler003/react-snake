e664ee34ab0bd57f307923a57325a4026b11e0a1
Bartek <b.pyzocha@gmail.com>
2022-04-03 12:40:34 +0200

    Add rounded corners

--- src/components/gameboard/Food.js
+++ src/components/gameboard/Food.js
@@ -2,5 +2,5 @@ import Block from './Block';
 import cls from './Food.module.css';
 
 export default function Food(props) {
-  return <Block className={cls.food} {...props} />;
+  return <Block className={cls.food} rounded='all' {...props} />;
 }
