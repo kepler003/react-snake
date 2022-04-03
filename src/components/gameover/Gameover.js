32207977eb44578f2b231bc2dbfd89c2a02490d6
Bartek <b.pyzocha@gmail.com>
2022-04-03 18:52:09 +0200

    Show final length from context

--- src/components/gameover/Gameover.js
+++ src/components/gameover/Gameover.js
@@ -1,11 +1,13 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useContext } from 'react';
 import { toClassName } from '../../utils/utils';
 import asCard from '../hoc/asCard';
 import Input from './../ui/Input';
 import Button from './../ui/Button';
 import cls from './Gameover.module.css';
+import Context from '../../store/store';
 
 function Gameover({ className }) {
+  const ctx = useContext(Context);
   const inputRef = useRef();
 
   useEffect(() => {
@@ -13,15 +15,21 @@ function Gameover({ className }) {
       inputRef.current.focus();
     }
   }, [inputRef.current]);
-  
+
   return (
     <div className={toClassName(className, cls.gameover)}>
-      <h1>Your snake reached</h1>
-      <p>25.5m</p>
+      <h1>Your snake is #12</h1>
+      <p>{ctx.snake.length / 100}m</p>
       <Input placeholder='Your name' ref={inputRef} />
-      <Button className={cls.saveBtn} theme='secondary' wide>Save</Button>
-      <Button theme='secondary' wide>Menu</Button>
-      <Button theme='secondary' wide>Try again</Button>
+      <Button className={cls.saveBtn} theme='secondary' wide>
+        Save
+      </Button>
+      <Button theme='secondary' wide>
+        Menu
+      </Button>
+      <Button theme='secondary' wide>
+        Try again
+      </Button>
     </div>
   );
 }
