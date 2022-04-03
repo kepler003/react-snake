648781536df00d44ac7c89057e7085ead624d047
Bartek <b.pyzocha@gmail.com>
2022-04-03 16:50:37 +0200

    Add wide btn

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,8 +1,21 @@
 import cls from './Button.module.css';
 import { toClassName } from '../../utils/utils';
 
-export default function Button({ children, className, theme, size, ...props }) {
-  const classNames = toClassName(className, cls.btn, cls[theme], cls[size]);
+export default function Button({
+  children,
+  className,
+  theme,
+  size,
+  wide,
+  ...props
+}) {
+  const classNames = toClassName(
+    className,
+    cls.btn,
+    cls[theme],
+    cls[size],
+    wide && cls.wide
+  );
   return (
     <button className={classNames} {...props}>
       {children}
