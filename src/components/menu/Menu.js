efc15649d828e24fbeccfd04486d8df4bcb53edb
Bartek <b.pyzocha@gmail.com>
2022-04-03 16:38:29 +0200

    Start playing

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,12 +3,16 @@ import asCard from './../hoc/asCard';
 import Button from './../ui/Button';
 import cls from './Menu.module.css';
 
-function Menu({ className, ...props }) {
+function Menu({ className, onPlay, ...props }) {
+  function play() {
+    onPlay();
+  }
+
   return (
     <div className={toClassName(className, cls.menu)} {...props}>
       <h1>SNAKE</h1>
       <Button theme='secondary'>Leaderboard</Button>
-      <Button theme='secondary'>Play</Button>
+      <Button theme='secondary' onClick={play}>Play</Button>
     </div>
   );
 }
