f2cec9bca5816fc7a8ee443018ea9fc81a51c949
Bartek <b.pyzocha@gmail.com>
2022-04-03 12:23:42 +0200

    Move rounded radius styling to Block component

--- src/components/gameboard/Block.js
+++ src/components/gameboard/Block.js
@@ -1,13 +1,25 @@
 import cls from './Block.module.css';
 import toClassName from './../../utils/toClassName';
 
-export default function Block({
-  className,
-  size = 21,
-  x,
-  y,
-}) {
-  const classNames = toClassName(className, cls.block);
+export default function Block({ className, size = 21, x, y, rounded }) {
+  const roundedClassNamesMap = {
+    rounded: cls.rounded,
+    top: cls.roundedTop,
+    bottom: cls.roundedBottom,
+    left: cls.roundedLeft,
+    right: cls.roundedRight,
+    'top left': cls.roundedTopLeft,
+    'top right': cls.roundedTopRight,
+    'bottom left': cls.roundedBottomLeft,
+    'bottom right': cls.roundedBottomRight,
+  };
+
+  const classNames = toClassName(
+    className,
+    cls.block,
+    roundedClassNamesMap[rounded]
+  );
+
   const style = {
     width: `calc(100% / ${size})`,
     height: `calc(100% / ${size})`,
