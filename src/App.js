fbb69a171a5dae26a5addf3a3c1d324da4efa94d
Bartek <b.pyzocha@gmail.com>
2022-04-03 16:39:22 +0200

    Add Menu component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import { ContextProvider } from './store/store';
 import Background from './components/layout/Background';
 import Main from './components/layout/Main';
 import Gameboard from './components/gameboard/Gameboard';
 import Header from './components/header/Header';
+import Menu from './components/menu/Menu';
 
 function App() {
+  const [view, setView] = useState('menu');
+
+  function play() {
+    setView('game');
+  }
+
+  const viewsMap = {
+    menu: <Menu onPlay={play} />,
+    game: <Gameboard />,
+  };
+
   return (
     <ContextProvider>
       <Background>
         <Main>
           <Header />
-          <Gameboard />
+          {viewsMap[view]}
         </Main>
       </Background>
     </ContextProvider>
