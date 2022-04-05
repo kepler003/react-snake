39b126c7e58dd3aaa9c20d5ebb588c2158766190
Bartek <b.pyzocha@gmail.com>
2022-04-05 22:00:14 +0200

    Move food functionality to Food component

--- src/components/game/Food.js
+++ src/components/game/Food.js
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
 import Block from './Block';
 import cls from './Food.module.css';
 
-export default function Food(props) {
-  if (props.x === null || props.y === null) {
-    return null;
-  } else {
-    return <Block className={cls.food} rounded='all' {...props} />;
+export default function Food({ snakeParts, onSpawn }) {
+  const [pos, setPos] = useState(null);
+
+  // Spawn food
+  useEffect(() => {
+    if (!pos && snakeParts) {
+      spawnFood();
+    }
+  }, [pos, snakeParts]);
+
+  // Lift food position up
+  useEffect(() => {
+    if (pos) {
+      onSpawn(pos);
+    }
+  }, [pos]);
+
+  // Check if snake is eating food
+  useEffect(() => {
+    if (checkIfIsEaten()) {
+      spawnFood();
+    }
+  }, [snakeParts]);
+
+  function spawnFood() {
+    let pos = null;
+
+    do {
+      const x = Math.floor(Math.random() * 21);
+      const y = Math.floor(Math.random() * 21);
+
+      const isInSnake = snakeParts?.some((snakePart) => {
+        return x === snakePart.x && y === snakePart.y;
+      });
+
+      if (!isInSnake) {
+        pos = { x, y };
+      }
+    } while (!pos);
+
+    setPos(pos);
   }
+
+  function checkIfIsEaten() {
+    const head = snakeParts?.[0];
+    return head?.x === pos?.x && head?.y === pos?.y;
+  }
+
+  return <Block className={cls.food} rounded='all' x={pos?.x} y={pos?.y} />;
 }
--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -9,59 +9,14 @@ function Game({ className, changeView }) {
   const [snakeParts, setSnakeParts] = useState(null);
   const [food, setFood] = useState(null);
 
-  // Placing the food
-  useEffect(() => {
-    if (food || !snakeParts) return;
-    addFood();
-  }, [food, snakeParts]);
-
-  // Eating food
-  useEffect(() => {
-    if (!food || !snakeParts) return;
-    if (!checkIfIsEating()) return;
-    removeFood();
-  }, [snakeParts]);
-
-  function addFood() {
-    let coords = null;
-
-    do {
-      const x = Math.floor(Math.random() * 21);
-      const y = Math.floor(Math.random() * 21);
-
-      const isInSnake = snakeParts.some((snakePart) => {
-        return x === snakePart.x && y === snakePart.y;
-      });
-
-      if (!isInSnake) {
-        coords = { x, y };
-      }
-    } while (!coords);
-
-    setFood(coords);
-  }
-
-  function removeFood() {
-    setFood(null);
-  }
-
-  function checkIfIsEating() {
-    const head = snakeParts[0];
-    return head.x === food.x && head.y === food.y;
-  }
-
-  function onMoveHandler(snakeParts) {
-    setSnakeParts(snakeParts);
-  }
-
-  function onGameOverHandler() {
-    changeView('end');
-  }
-
   return (
     <div className={toClassName(className, cls.game)}>
-      <Snake onMove={onMoveHandler} onGameOver={onGameOverHandler} food={food} />
-      <Food {...food} />
+      <Snake
+        food={food}
+        onMove={setSnakeParts}
+        onGameOver={() => changeView('end')}
+      />
+      <Food snakeParts={snakeParts} onSpawn={setFood} />
     </div>
   );
 }
