2afbc3f75fa1bf61cbb71ba0b634bd49d2451df4
Bartek <b.pyzocha@gmail.com>
2022-04-05 21:35:02 +0200

    Fix not showing food when one of coords === 0

--- src/components/game/Food.js
+++ src/components/game/Food.js
@@ -2,7 +2,7 @@ import Block from './Block';
 import cls from './Food.module.css';
 
 export default function Food(props) {
-  if (!props.x || !props.y) {
+  if (props.x === null || props.y === null) {
     return null;
   } else {
     return <Block className={cls.food} rounded='all' {...props} />;
