58e320efc1318f328c522303f9569220bfad7701
Bartek <b.pyzocha@gmail.com>
2022-04-05 19:44:37 +0200

    Refactor component

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,30 +1,25 @@
 import cls from './Button.module.css';
 import { toClassName } from '../../utils/utils';
 
-export default function Button({
-  children,
-  className,
-  theme,
-  size,
-  wide,
-  onClick,
-  ...props
-}) {
+export default function Button(props) {
+  const { children, className, theme, size, wide, onClick, ...restProps } =
+    props;
+
   const classNames = toClassName(
     className,
     cls.btn,
     cls[theme],
     cls[size],
-    wide && cls.wide
+    wide ? cls.wide : ''
   );
 
   function onClickHandler(e) {
     e.target.blur();
     onClick && onClick(e);
   }
-  
+
   return (
-    <button className={classNames} onClick={onClickHandler} {...props}>
+    <button className={classNames} onClick={onClickHandler} {...restProps}>
       {children}
     </button>
   );
